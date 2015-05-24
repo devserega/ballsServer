63016a12830f27d2548129ea0f004b8575cc4807
dsserega <dsserega@gmail.com>
2015-05-24 22:51:54 +0300

    update

--- git_push.sh
+++ git_push.sh
@@ -1,7 +1,4 @@
 #!/bin/sh
-rem git config --global credential.helper cache
-git config --global user.name "dsserega"
-git config --global user.email "dsserega@gmail.com"
 git add .
 git commit -a -m "update"
 git push origin master
