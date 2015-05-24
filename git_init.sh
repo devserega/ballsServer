70a81f02439f574c6e0fb21329253bf1a567ac87
dsserega <dsserega@gmail.com>
2015-05-24 22:51:30 +0300

    update

---
+++ git_init.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+git init
+git add .
+git commit -m "first commit"
+git remote add origin https://github.com/dsserega/ballsServer.git
+git push -u origin master
+
---
+++ git_pull.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+git pull https://github.com/dsserega/ballsServer.git
