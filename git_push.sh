37605091d4d545de502153cef93292a7a5da2b9c
serega <dsserega@gmail.com>
2015-05-26 18:19:07 +0300

    update

--- git_push.sh
+++ git_push.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 git add .
 git commit -a -m "update"
+git config –-global user.name dsserega
+git config user.name dsserega
 git remote add origin https://github.com/dsserega/ballsServer.git
 git push -u origin master
