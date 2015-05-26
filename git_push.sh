d06fb9e396cff3abad73286b99b77fb2a92fdeef
dsserega <dsserega@gmail.com>
2015-05-26 18:25:12 +0300

    update

--- git_push.sh
+++ git_push.sh
@@ -3,6 +3,6 @@ git add .
 git commit -a -m "update"
 git config –-global user.email "dsserega@gmail.com"
 git config --global user.name "dsserega"
-git remote rm origin
+#git remote rm origin
 git remote add origin git@github.com:dsserega/ballsServer.git
 git push -u origin master
